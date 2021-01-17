af6b8f4b63b10c6714771dbb57989017a986d29e
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-01-17 15:14:07 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,31 @@
+// Zadanie 2.1:
+num = 12;
+st = "napis";
+bool = true;
+n = null;
+combo = number + string;
+
+console.log(num);
+console.log(str);
+console.log(combo);
+console.log(bool);
+console.log(n);
+
+// Zadanie 2.2
+
+let num = 1,
+    num1 = 2,
+    sum = 0;
+
+sum = num + num1;
+
+console.log(sum);
+
+//Zadanie 2.3
+let x;
+console.log(x); // brak przypisanej wartości do zmiennej
+
+
+
+
+

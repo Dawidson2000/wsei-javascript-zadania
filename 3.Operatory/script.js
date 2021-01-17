80a498d48957363756e81a501c86b58ea330be91
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-01-17 15:14:24 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,46 @@
+//Zdanie 3.1
+let x = true,
+    y = false;
+console.log(x==y); // wyświetli false
+
+//Zadanie 3.2
+const numOne = 10;
+const numTwo = 3;
+
+let moduloResult = 0;
+moduloResult = numOne % numTwo;
+console.log(moduloResult); //1
+
+//Zadanie 3.3
+let str1 = "Front",
+    str2 = "end",
+    stringsResult = "";
+
+    stringsResult = str1 + str2;
+    console.log(stringsResult);
+ 
+//Zadanie 3.4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber==someString); // true poniewaz wartosc jest ta sama
+console.log(someNumber===someString); // false ponieważ jest inny typ wartosci
+
+//Zadanie 3.5
+let counter = 30;
+console.log(counter); //30
+counter++;
+console.log(counter); //31
+counter--;
+console.log(counter); //30
+
+//Zadanie 3.6
+
+let x = 3,
+    y= 7,
+    result = null;
+    
+    result = x > y;
+
+    console.log(result); // false
+
